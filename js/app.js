4972f72d3f1928b341a09915dfe91223cc431425
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-01-01 12:36:48 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+const STORAGE_KEY = "smartform_form";
 
+function getForm() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+}
+
+function saveFormData(data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
